677ef6d43cb838744ed6b06ba353ba885e613c1c
Kunal Hemani <kunalnhemani@gmail.com>
2024-02-01 20:33:24 +0530

    Hotel Management System

---
+++ Hotel Management System/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.rid = New System.Windows.Forms.TextBox()
+        Me.nm = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.mob_no = New System.Windows.Forms.TextBox()
+        Me.btn_insert = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_search = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(84, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Room ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(84, 139)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Customer's Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(84, 182)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Customer's City"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(84, 224)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(160, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Customer's Mob_No"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(84, 268)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Check in"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(84, 312)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Check out"
+        '
+        'rid
+        '
+        Me.rid.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rid.Location = New System.Drawing.Point(268, 88)
+        Me.rid.Name = "rid"
+        Me.rid.Size = New System.Drawing.Size(121, 28)
+        Me.rid.TabIndex = 6
+        '
+        'nm
+        '
+        Me.nm.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nm.Location = New System.Drawing.Point(268, 131)
+        Me.nm.Name = "nm"
+        Me.nm.Size = New System.Drawing.Size(121, 28)
+        Me.nm.TabIndex = 7
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(268, 260)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(188, 28)
+        Me.DateTimePicker1.TabIndex = 8
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker2.Location = New System.Drawing.Point(268, 304)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(188, 28)
+        Me.DateTimePicker2.TabIndex = 9
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Surat", "Mumbai", "Goa", "Bangalore"})
+        Me.ComboBox1.Location = New System.Drawing.Point(268, 174)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox1.TabIndex = 10
+        '
+        'mob_no
+        '
+        Me.mob_no.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mob_no.Location = New System.Drawing.Point(268, 216)
+        Me.mob_no.Name = "mob_no"
+        Me.mob_no.Size = New System.Drawing.Size(121, 28)
+        Me.mob_no.TabIndex = 11
+        '
+        'btn_insert
+        '
+        Me.btn_insert.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_insert.Location = New System.Drawing.Point(88, 369)
+        Me.btn_insert.Name = "btn_insert"
+        Me.btn_insert.Size = New System.Drawing.Size(101, 35)
+        Me.btn_insert.TabIndex = 12
+        Me.btn_insert.Text = "Book"
+        Me.btn_insert.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_update.Location = New System.Drawing.Point(268, 369)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(90, 35)
+        Me.btn_update.TabIndex = 13
+        Me.btn_update.Text = "Update"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_delete.Location = New System.Drawing.Point(88, 451)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(101, 34)
+        Me.btn_delete.TabIndex = 14
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_search
+        '
+        Me.btn_search.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_search.Location = New System.Drawing.Point(268, 451)
+        Me.btn_search.Name = "btn_search"
+        Me.btn_search.Size = New System.Drawing.Size(90, 34)
+        Me.btn_search.TabIndex = 15
+        Me.btn_search.Text = "Search"
+        Me.btn_search.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(509, 106)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(837, 405)
+        Me.DataGridView1.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 22.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(81, 20)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(353, 42)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "GATEWAY HOTEL "
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(863, 50)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(170, 32)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Booking List"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.ClientSize = New System.Drawing.Size(1358, 533)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btn_search)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.btn_insert)
+        Me.Controls.Add(Me.mob_no)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.nm)
+        Me.Controls.Add(Me.rid)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents rid As TextBox
+    Friend WithEvents nm As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents mob_no As TextBox
+    Friend WithEvents btn_insert As Button
+    Friend WithEvents btn_update As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_search As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Hotel Management System/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub btn_insert_Click(sender As Object, e As EventArgs) Handles btn_insert.Click
+        Dim con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kunal\source\repos\Hotel Management System\Hotel Management System\hotel_db.mdf;Integrated Security=True")
+
+        con.Open()
+        Dim cmd As SqlCommand = New SqlCommand("insert into users values ('" & rid.Text & "', '" & nm.Text & "', '" & ComboBox1.Text & "', '" & mob_no.Text & "', '" & DateTimePicker1.Text & "', '" & DateTimePicker2.Text & "')", con)
+        cmd.Connection = con
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Data inserted")
+        bindData()
+    End Sub
+
+    Private Sub bindData()
+        Dim ds As New DataSet
+        ds.Clear()
+        Dim con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kunal\source\repos\Hotel Management System\Hotel Management System\hotel_db.mdf;Integrated Security=True")
+
+        con.Open()
+        Dim cmd As SqlCommand = New SqlCommand("select * from users", con)
+        Dim sda As New SqlDataAdapter(cmd)
+        sda.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+    End Sub
+
+    Private Sub btn_update_Click(sender As Object, e As EventArgs) Handles btn_update.Click
+        Dim query As String = "update users set cust_name = '" & nm.Text & "', cust_city = '" & ComboBox1.Text & "',cust_no = '" & mob_no.Text & "', check_in = '" & DateTimePicker1.Text & "', check_out = '" & DateTimePicker2.Text & "' where room_id = '" & rid.Text & "'"
+        Dim con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kunal\source\repos\Hotel Management System\Hotel Management System\hotel_db.mdf;Integrated Security=True")
+        Dim cmd As SqlCommand = New SqlCommand(query, con)
+
+        con.Open()
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Updated Successfully")
+        bindData()
+    End Sub
+
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        Dim query As String = "delete from users where room_id = '" & rid.Text & "'"
+        Dim con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kunal\source\repos\Hotel Management System\Hotel Management System\hotel_db.mdf;Integrated Security=True")
+
+        con.Open()
+        Dim cmd As New SqlCommand(query, con)
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Deleted Successfully")
+        bindData()
+    End Sub
+
+    Private Sub btn_search_Click(sender As Object, e As EventArgs) Handles btn_search.Click
+        Dim query As String = "select * from users where room_id = '" & rid.Text & "'"
+        Dim con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kunal\source\repos\Hotel Management System\Hotel Management System\hotel_db.mdf;Integrated Security=True")
+        Dim cmd As SqlCommand = New SqlCommand(query, con)
+
+        Dim sda As New SqlDataAdapter()
+        sda.SelectCommand = cmd
+
+        Dim dt As New DataTable()
+        sda.Fill(dt)
+        If dt.Rows.Count > 0 Then
+            nm.Text = dt.Rows(0)(1).ToString()
+            ComboBox1.Text = dt.Rows(0)(2).ToString()
+            mob_no.Text = dt.Rows(0)(3).ToString()
+            DateTimePicker1.Text = dt.Rows(0)(4).ToString()
+            DateTimePicker2.Text = dt.Rows(0)(5).ToString()
+        End If
+        bindData()
+    End Sub
+End Class
